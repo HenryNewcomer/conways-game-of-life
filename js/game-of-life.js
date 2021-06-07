a0b954010761bb2a3590a69f1777b02a9190c183
Henry Newcomer <a.cliche.email@gmail.com>
2021-06-06 21:09:44 -0500

    Change points to squares

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -5,7 +5,7 @@
 const UNIT_SIZE = 10;
 
 // Container
-var units = [[0, 0], [10, 10], [15, 20], [100, 100]];
+var units = [[0, 0], [50, 50], [150, 150], [200, 200]];
 
 function draw() {
     background("#3399ff");
@@ -16,11 +16,16 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
 }
 
+function checkNeighbors() {
+    let neighborsToCheck = [];
+    //
+}
+
 function drawUnits() {
     stroke("#0f0");
     strokeWeight(UNIT_SIZE);
     for (let i = 0; i < units.length; i++) {
-        point(units[i][0], units[i][1]);
+        square(units[i][0], units[i][1], UNIT_SIZE, 2);
         console.log("unit "+ i + ": " + units[i]);
     }
 }
