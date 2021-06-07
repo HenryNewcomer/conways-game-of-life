9f4d76e8ef1df8ee9d85209fa294f6efbb43fd0e
Henry Newcomer <a.cliche.email@gmail.com>
2021-06-06 20:50:07 -0500

    Draw points on canvas

--- js/game-of-life.js
+++ js/game-of-life.js
@@ -2,9 +2,25 @@
  * Author: Henry Newcomer *
  **************************/
 
+const UNIT_SIZE = 10;
+
+// Container
+var units = [[0, 0], [10, 10], [15, 20], [100, 100]];
+
 function draw() {
+    background("#3399ff");
+    drawUnits();
 }
 
 function setup() {
-    console.log("Starting...");
+    createCanvas(windowWidth, windowHeight);
+}
+
+function drawUnits() {
+    stroke("#0f0");
+    strokeWeight(UNIT_SIZE);
+    for (let i = 0; i < units.length; i++) {
+        point(units[i][0], units[i][1]);
+        console.log("unit "+ i + ": " + units[i]);
+    }
 }
